318463ff0790015cf5150ce35262fc5b4ee167a4
Paulo Gomes <paullo.h3n@gmail.com>
2020-05-02 10:01:23 -0300

    Challenge solved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import {
   SafeAreaView,
@@ -10,46 +10,64 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import api from "./services/api";
+
 export default function App() {
-  async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
-  }
+  const [repositories, setRepositories] = useState([])
+
+  const handleLikeRepository = (id) => {
+    api.post(`/repositories/${id}/like`)
+      .then(response => {
+        const repository = response.data;
+
+        const list = repositories.map(item => item.id === id ? repository : item);
+
+        setRepositories(list);
+      })
+  } 
+  
+  useEffect(() => {
+    api.get("/repositories").then(response => setRepositories(response.data));
+  }, [])
+
+  const renderTechs = (techs) => 
+    techs.map((tech, index) => <Text key={index} style={styles.tech}>{tech}</Text>);
+
+  const renderItem = ({id, title, techs, likes}) => (
+    <View style={styles.repositoryContainer}>
+      <Text style={styles.repository}>{title}</Text>
+
+      <View style={styles.techsContainer}>
+        {renderTechs(techs)}
+      </View>
+
+      <View style={styles.likesContainer}>
+        <Text
+          style={styles.likeText}
+          testID={`repository-likes-${id}`}
+        >
+          {likes} curtidas
+        </Text>
+      </View>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => handleLikeRepository(id)}
+        testID={`like-button-${id}`}
+      >
+        <Text style={styles.buttonText}>Curtir</Text>
+      </TouchableOpacity>
+    </View>
+  );
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
-
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+        <FlatList
+          data={repositories}
+          renderItem={({item}) => renderItem(item)}
+        />        
       </SafeAreaView>
     </>
   );
@@ -65,6 +83,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 15,
     backgroundColor: "#fff",
     padding: 20,
+    borderRadius: 10
   },
   repository: {
     fontSize: 32,
@@ -72,7 +91,7 @@ const styles = StyleSheet.create({
   },
   techsContainer: {
     flexDirection: "row",
-    marginTop: 10,
+    marginTop: 10
   },
   tech: {
     fontSize: 12,
@@ -82,6 +101,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 5,
     color: "#fff",
+    borderRadius: 10
   },
   likesContainer: {
     marginTop: 15,
@@ -94,7 +114,7 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   button: {
-    marginTop: 10,
+    marginTop: 10
   },
   buttonText: {
     fontSize: 14,
@@ -103,5 +123,6 @@ const styles = StyleSheet.create({
     color: "#fff",
     backgroundColor: "#7159c1",
     padding: 15,
+    borderRadius: 10
   },
 });
